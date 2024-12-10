e1bb094304035c3e7e4217dd9ae1976dd9f44959
rpardiola <pardiola85@gmail.com>
2024-12-10 20:39:22 +0100

    added second screen for the result

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -36,6 +37,11 @@ class MainActivity : AppCompatActivity() {
 
                 val alturaq2 = altura * altura
                 val resultado = peso / alturaq2
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
                 println (resultado)
 
             }
