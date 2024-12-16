d86f326e88e0c5aef334a58590730eeb8beb9411
rpardiola <pardiola85@gmail.com>
2024-12-16 22:54:09 +0100

    improving layout

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra(KEY_RESULT_IMC, resultado)
                 startActivity(intent)
 
-                println (resultado)
+
 
             }
 
