a66a5576d3d60686c5ae681947dc4e332f10bff8
ricardop <pardiola85@gmail.com>
2024-12-23 12:55:46 +0100

    adicionado cor a classificacao

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -27,20 +27,20 @@ class ResultActivity : AppCompatActivity() {
 
         if(result <= 18.5f){
             classificacao = "MAGREZA"
-            corClassificacao = "#00BFFF" // Cor azul claro
+            corClassificacao = "#00BFFF" // Cor Azul Claro
         } else if(result > 18.5f && result <= 24.9f){
             classificacao = "NORMAL"
-            corClassificacao = "#32CD32" // Cor verde
+            corClassificacao = "#32CD32" // Cor Verde
         } else if(result >25f && result <= 29.9f){
             classificacao = "SOBREPESO"
-            corClassificacao = "#FFA500" // Cor laranja
+            corClassificacao = "#FFA500" // Cor Laranja
         } else if(result > 30f && result <= 39.9f){
             classificacao = "OBESIDADE"
-            corClassificacao = "#FF4500" // Cor vermelho laranja
+            corClassificacao = "#FF4500" // Cor Vermelho laranja
         } else {
 
             classificacao = "OBESIDADE GRAVE"
-            corClassificacao = "#FF0000" // Cor vermelha
+            corClassificacao = "#FF0000" // Cor Vermelha
 
         }
         tvClassificacao.text = classificacao
